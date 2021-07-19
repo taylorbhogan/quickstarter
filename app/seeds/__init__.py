ba9f226b6355398c16a9f42666356507bfa235cb
Taylor Hogan <taylorb.hogan@gmail.com>
2021-07-19 16:08:16 -0700

    Add country seeds

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,6 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .countries import seed_countries, undo_countries
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -10,6 +11,7 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     seed_users()
+    seed_countries()
     # Add other seed functions here
 
 
@@ -17,4 +19,5 @@ def seed():
 @seed_commands.command('undo')
 def undo():
     undo_users()
+    undo_countries()
     # Add other undo functions here
---
+++ app/seeds/countries.py
@@ -0,0 +1,549 @@
+from app.models import db, Country
+
+
+# Adds demo countries from Wikipedia
+def seed_countries():
+    Abkhazia = Country(name='Abkhazia')
+    Afghanistan = Country(name='Afghanistan')
+    AkrotiriAndDhekelia = Country(name='Akrotiri and Dhekelia')
+    ÅlandIslands = Country(name='Åland Islands')
+    Albania = Country(name='Albania')
+    Algeria = Country(name='Algeria')
+    AmericanSamoa = Country(name='American Samoa')
+    Andorra = Country(name='Andorra')
+    Angola = Country(name='Angola')
+    Anguilla = Country(name='Anguilla')
+    Antarctica = Country(name='Antarctica')
+    AntiguaAndBarbuda = Country(name='Antigua and Barbuda')
+    Argentina = Country(name='Argentina')
+    Armenia = Country(name='Armenia')
+    Aruba = Country(name='Aruba')
+    AshmoreAndCartierIslands = Country(name='Ashmore and Cartier Islands')
+    Australia = Country(name='Australia')
+    Austria = Country(name='Austria')
+    Azerbaijan = Country(name='Azerbaijan')
+    Bahamas = Country(name='Bahamas')
+    Bahrain = Country(name='Bahrain')
+    Bangladesh = Country(name='Bangladesh')
+    Barbados = Country(name='Barbados')
+    Belarus = Country(name='Belarus')
+    Belgium = Country(name='Belgium')
+    Belize = Country(name='Belize')
+    Benin = Country(name='Benin')
+    Bermuda = Country(name='Bermuda')
+    Bhutan = Country(name='Bhutan')
+    BirTawil = Country(name='Bir Tawil')
+    Bolivia = Country(name='Bolivia')
+    Bonaire = Country(name='Bonaire')
+    BosniaAndHerzegovina = Country(name='Bosnia and Herzegovina')
+    Botswana = Country(name='Botswana')
+    BouvetIsland = Country(name='Bouvet Island')
+    Brazil = Country(name='Brazil')
+    BritishIndianOceanTerritory = Country(name='British Indian Ocean Territory')
+    BritishVirginIslands = Country(name='British Virgin Islands')
+    Brunei = Country(name='Brunei')
+    Bulgaria = Country(name='Bulgaria')
+    BurkinaFaso = Country(name='Burkina Faso')
+    Burundi = Country(name='Burundi')
+    Cambodia = Country(name='Cambodia')
+    Cameroon = Country(name='Cameroon')
+    Canada = Country(name='Canada')
+    CapeVerde = Country(name='Cape Verde')
+    CaymanIslands = Country(name='Cayman Islands')
+    CentralAfricanRepublic = Country(name='Central African Republic')
+    Chad = Country(name='Chad')
+    Chile = Country(name='Chile')
+    China = Country(name='China')
+    ChristmasIsland = Country(name='Christmas Island')
+    ClippertonIsland = Country(name='Clipperton Island')
+    CocosIslands = Country(name='Cocos Islands')
+    Colombia = Country(name='Colombia')
+    Comoros = Country(name='Comoros')
+    Congo = Country(name='Congo')
+    CookIslands = Country(name='Cook Islands')
+    CoralSeaIslands = Country(name='Coral Sea Islands')
+    CostaRica = Country(name='Costa Rica')
+    Croatia = Country(name='Croatia')
+    Cuba = Country(name='Cuba')
+    Curaçao = Country(name='Curaçao')
+    Cyprus = Country(name='Cyprus')
+    CzechRepublic = Country(name='Czech Republic')
+    Denmark = Country(name='Denmark')
+    Djibouti = Country(name='Djibouti')
+    Dominica = Country(name='Dominica')
+    DominicanRepublic = Country(name='Dominican Republic')
+    DRCongo = Country(name='DR Congo')
+    EastTimor = Country(name='East Timor')
+    Ecuador = Country(name='Ecuador')
+    Egypt = Country(name='Egypt')
+    ElSalvador = Country(name='El Salvador')
+    EquatorialGuinea = Country(name='Equatorial Guinea')
+    Eritrea = Country(name='Eritrea')
+    Estonia = Country(name='Estonia')
+    Eswatini = Country(name='Eswatini')
+    Ethiopia = Country(name='Ethiopia')
+    FalklandIslands = Country(name='Falkland Islands')
+    FaroeIslands = Country(name='Faroe Islands')
+    Fiji = Country(name='Fiji')
+    Finland = Country(name='Finland')
+    France = Country(name='France')
+    France = Country(name='France')
+    FrenchPolynesia = Country(name='French Polynesia')
+    FrenchSouthernTerritories = Country(name='French Southern Territories')
+    Gabon = Country(name='Gabon')
+    Gambia = Country(name='Gambia')
+    Georgia = Country(name='Georgia')
+    Germany = Country(name='Germany')
+    Ghana = Country(name='Ghana')
+    Gibraltar = Country(name='Gibraltar')
+    Greece = Country(name='Greece')
+    Greenland = Country(name='Greenland')
+    Grenada = Country(name='Grenada')
+    Guam = Country(name='Guam')
+    Guatemala = Country(name='Guatemala')
+    Guernsey = Country(name='Guernsey')
+    Guinea = Country(name='Guinea')
+    GuineaBissau = Country(name='Guinea-Bissau')
+    Guyana = Country(name='Guyana')
+    Haiti = Country(name='Haiti')
+    HeardIslandAndMcDonaldIslands = Country(name='Heard Island and McDonald Islands')
+    Honduras = Country(name='Honduras')
+    HongKong = Country(name='Hong Kong')
+    Hungary = Country(name='Hungary')
+    Iceland = Country(name='Iceland')
+    India = Country(name='India')
+    Indonesia = Country(name='Indonesia')
+    Iran = Country(name='Iran')
+    Iraq = Country(name='Iraq')
+    Ireland = Country(name='Ireland')
+    IsleofMan = Country(name='Isle of Man')
+    Israel = Country(name='Israel')
+    Italy = Country(name='Italy')
+    IvoryCoast = Country(name='Ivory Coast')
+    Jamaica = Country(name='Jamaica')
+    JanMayen = Country(name='Jan Mayen')
+    Japan = Country(name='Japan')
+    Jersey = Country(name='Jersey')
+    Jordan = Country(name='Jordan')
+    Kazakhstan = Country(name='Kazakhstan')
+    Kenya = Country(name='Kenya')
+    KingdomOfDenmark = Country(name='Kingdom of Denmark')
+    Kiribati = Country(name='Kiribati')
+    Kosovo = Country(name='Kosovo')
+    Kuwait = Country(name='Kuwait')
+    Kyrgyzstan = Country(name='Kyrgyzstan')
+    Laos = Country(name='Laos')
+    Latvia = Country(name='Latvia')
+    Lebanon = Country(name='Lebanon')
+    Lesotho = Country(name='Lesotho')
+    Liberia = Country(name='Liberia')
+    Libya = Country(name='Libya')
+    Liechtenstein = Country(name='Liechtenstein')
+    Lithuania = Country(name='Lithuania')
+    Luxembourg = Country(name='Luxembourg')
+    Macau = Country(name='Macau')
+    Madagascar = Country(name='Madagascar')
+    Malawi = Country(name='Malawi')
+    Malaysia = Country(name='Malaysia')
+    Maldives = Country(name='Maldives')
+    Mali = Country(name='Mali')
+    Malta = Country(name='Malta')
+    MarshallIslands = Country(name='Marshall Islands')
+    Mauritania = Country(name='Mauritania')
+    Mauritius = Country(name='Mauritius')
+    Mexico = Country(name='Mexico')
+    Micronesia = Country(name='Micronesia')
+    Moldova = Country(name='Moldova')
+    Monaco = Country(name='Monaco')
+    Mongolia = Country(name='Mongolia')
+    Montenegro = Country(name='Montenegro')
+    Montserrat = Country(name='Montserrat')
+    Morocco = Country(name='Morocco')
+    Mozambique = Country(name='Mozambique')
+    Myanmar = Country(name='Myanmar')
+    Namibia = Country(name='Namibia')
+    Nauru = Country(name='Nauru')
+    Nepal = Country(name='Nepal')
+    Netherlands = Country(name='Netherlands')
+    NewCaledonia = Country(name='New Caledonia')
+    NewZealand = Country(name='New Zealand')
+    Nicaragua = Country(name='Nicaragua')
+    Niger = Country(name='Niger')
+    Nigeria = Country(name='Nigeria')
+    Niue = Country(name='Niue')
+    NorfolkIsland = Country(name='Norfolk Island')
+    NorthKorea = Country(name='North Korea')
+    NorthMacedonia = Country(name='North Macedonia')
+    NorthernCyprus = Country(name='Northern Cyprus')
+    NorthernMarianaIslands = Country(name='Northern Mariana Islands')
+    Norway = Country(name='Norway')
+    Norway = Country(name='Norway')
+    Oman = Country(name='Oman')
+    Pakistan = Country(name='Pakistan')
+    Palau = Country(name='Palau')
+    Palestine = Country(name='Palestine')
+    Panama = Country(name='Panama')
+    PapuaNewGuinea = Country(name='Papua New Guinea')
+    Paraguay = Country(name='Paraguay')
+    Peru = Country(name='Peru')
+    Philippines = Country(name='Philippines')
+    PitcairnIslands = Country(name='Pitcairn Islands')
+    Poland = Country(name='Poland')
+    Portugal = Country(name='Portugal')
+    PuertoRico = Country(name='Puerto Rico')
+    Qatar = Country(name='Qatar')
+    RepublicofArtsakh = Country(name='Republic of Artsakh')
+    Romania = Country(name='Romania')
+    Russia = Country(name='Russia')
+    Rwanda = Country(name='Rwanda')
+    Saba = Country(name='Saba')
+    SahrawiArabDemocraticRepublic = Country(name='Sahrawi Arab Democratic Republic')
+    SaintBarthélemy = Country(name='Saint Barthélemy')
+    SaintHelenaAscensionAndTristanDaCunha = Country(name='Saint Helena, Ascension and Tristan da Cunha')
+    SaintKittsAndNevis = Country(name='Saint Kitts and Nevis')
+    SaintLucia = Country(name='Saint Lucia')
+    SaintMartin = Country(name='Saint Martin')
+    SaintPierreAndMiquelon = Country(name='Saint Pierre and Miquelon')
+    SaintVincentAndTheGrenadines = Country(name='Saint Vincent and the Grenadines')
+    Samoa = Country(name='Samoa')
+    SanMarino = Country(name='San Marino')
+    SãoToméAndPríncipe = Country(name='São Tomé and Príncipe')
+    SaudiArabia = Country(name='Saudi Arabia')
+    Senegal = Country(name='Senegal')
+    Serbia = Country(name='Serbia')
+    Seychelles = Country(name='Seychelles')
+    SierraLeone = Country(name='Sierra Leone')
+    Singapore = Country(name='Singapore')
+    SintEustatius = Country(name='Sint Eustatius')
+    SintMaarten = Country(name='Sint Maarten')
+    Slovakia = Country(name='Slovakia')
+    Slovenia = Country(name='Slovenia')
+    SolomonIslands = Country(name='Solomon Islands')
+    Somalia = Country(name='Somalia')
+    Somaliland = Country(name='Somaliland')
+    SouthAfrica = Country(name='South Africa')
+    SouthGeorgiaAndTheSouthSandwichIslands = Country(name='South Georgia and the South Sandwich Islands')
+    SouthKorea = Country(name='South Korea')
+    SouthOssetia = Country(name='South Ossetia')
+    SouthSudan = Country(name='South Sudan')
+    Spain = Country(name='Spain')
+    SpratlyIslands = Country(name='Spratly Islands')
+    SriLanka = Country(name='Sri Lanka')
+    Sudan = Country(name='Sudan')
+    Suriname = Country(name='Suriname')
+    Svalbard = Country(name='Svalbard')
+    Sweden = Country(name='Sweden')
+    Switzerland = Country(name='Switzerland')
+    Syria = Country(name='Syria')
+    Taiwan = Country(name='Taiwan')
+    Tajikistan = Country(name='Tajikistan')
+    Tanzania = Country(name='Tanzania')
+    Thailand = Country(name='Thailand')
+    Togo = Country(name='Togo')
+    Tokelau = Country(name='Tokelau')
+    Tonga = Country(name='Tonga')
+    Transnistria = Country(name='Transnistria')
+    TrinidadAndTobago = Country(name='Trinidad and Tobago')
+    Tunisia = Country(name='Tunisia')
+    Turkey = Country(name='Turkey')
+    Turkmenistan = Country(name='Turkmenistan')
+    TurksAndCaicosIslands = Country(name='Turks and Caicos Islands')
+    Tuvalu = Country(name='Tuvalu')
+    USVirginIslands = Country(name='U.S. Virgin Islands')
+    Uganda = Country(name='Uganda')
+    Ukraine = Country(name='Ukraine')
+    UnitedArabEmirates = Country(name='United Arab Emirates')
+    UnitedKingdom = Country(name='United Kingdom')
+    UnitedStates = Country(name='United States')
+    UnitedStatesMinorOutlyingIslands = Country(name='United States Minor Outlying Islands')
+    Uruguay = Country(name='Uruguay')
+    Uzbekistan = Country(name='Uzbekistan')
+    Vanuatu = Country(name='Vanuatu')
+    VaticanCity = Country(name='Vatican City')
+    Venezuela = Country(name='Venezuela')
+    Vietnam = Country(name='Vietnam')
+    WallisAndFutuna = Country(name='Wallis and Futuna')
+    Yemen = Country(name='Yemen')
+    Zambia = Country(name='Zambia')
+    Zimbabwe = Country(name='Zimbabwe')
+
+    countries = [
+        Abkhazia,
+        Afghanistan,
+        AkrotiriAndDhekelia,
+        ÅlandIslands,
+        Albania,
+        Algeria,
+        AmericanSamoa,
+        Andorra,
+        Angola,
+        Anguilla,
+        Antarctica,
+        AntiguaAndBarbuda,
+        Argentina,
+        Armenia,
+        Aruba,
+        AshmoreAndCartierIslands,
+        Australia,
+        Austria,
+        Azerbaijan,
+        Bahamas,
+        Bahrain,
+        Bangladesh,
+        Barbados,
+        Belarus,
+        Belgium,
+        Belize,
+        Benin,
+        Bermuda,
+        Bhutan,
+        BirTawil,
+        Bolivia,
+        Bonaire,
+        BosniaAndHerzegovina,
+        Botswana,
+        BouvetIsland,
+        Brazil,
+        BritishIndianOceanTerritory,
+        BritishVirginIslands,
+        Brunei,
+        Bulgaria,
+        BurkinaFaso,
+        Burundi,
+        Cambodia,
+        Cameroon,
+        Canada,
+        CapeVerde,
+        CaymanIslands,
+        CentralAfricanRepublic,
+        Chad,
+        Chile,
+        China,
+        ChristmasIsland,
+        ClippertonIsland,
+        CocosIslands,
+        Colombia,
+        Comoros,
+        Congo,
+        CookIslands,
+        CoralSeaIslands,
+        CostaRica,
+        Croatia,
+        Cuba,
+        Curaçao,
+        Cyprus,
+        CzechRepublic,
+        Denmark,
+        Djibouti,
+        Dominica,
+        DominicanRepublic,
+        DRCongo,
+        EastTimor,
+        Ecuador,
+        Egypt,
+        ElSalvador,
+        EquatorialGuinea,
+        Eritrea,
+        Estonia,
+        Eswatini,
+        Ethiopia,
+        FalklandIslands,
+        FaroeIslands,
+        Fiji,
+        Finland,
+        France,
+        France,
+        FrenchPolynesia,
+        FrenchSouthernTerritories,
+        Gabon,
+        Gambia,
+        Georgia,
+        Germany,
+        Ghana,
+        Gibraltar,
+        Greece,
+        Greenland,
+        Grenada,
+        Guam,
+        Guatemala,
+        Guernsey,
+        Guinea,
+        GuineaBissau,
+        Guyana,
+        Haiti,
+        HeardIslandAndMcDonaldIslands,
+        Honduras,
+        HongKong,
+        Hungary,
+        Iceland,
+        India,
+        Indonesia,
+        Iran,
+        Iraq,
+        Ireland,
+        IsleofMan,
+        Israel,
+        Italy,
+        IvoryCoast,
+        Jamaica,
+        JanMayen,
+        Japan,
+        Jersey,
+        Jordan,
+        Kazakhstan,
+        Kenya,
+        KingdomOfDenmark,
+        Kiribati,
+        Kosovo,
+        Kuwait,
+        Kyrgyzstan,
+        Laos,
+        Latvia,
+        Lebanon,
+        Lesotho,
+        Liberia,
+        Libya,
+        Liechtenstein,
+        Lithuania,
+        Luxembourg,
+        Macau,
+        Madagascar,
+        Malawi,
+        Malaysia,
+        Maldives,
+        Mali,
+        Malta,
+        MarshallIslands,
+        Mauritania,
+        Mauritius,
+        Mexico,
+        Micronesia,
+        Moldova,
+        Monaco,
+        Mongolia,
+        Montenegro,
+        Montserrat,
+        Morocco,
+        Mozambique,
+        Myanmar,
+        Namibia,
+        Nauru,
+        Nepal,
+        Netherlands,
+        NewCaledonia,
+        NewZealand,
+        Nicaragua,
+        Niger,
+        Nigeria,
+        Niue,
+        NorfolkIsland,
+        NorthKorea,
+        NorthMacedonia,
+        NorthernCyprus,
+        NorthernMarianaIslands,
+        Norway,
+        Norway,
+        Oman,
+        Pakistan,
+        Palau,
+        Palestine,
+        Panama,
+        PapuaNewGuinea,
+        Paraguay,
+        Peru,
+        Philippines,
+        PitcairnIslands,
+        Poland,
+        Portugal,
+        PuertoRico,
+        Qatar,
+        RepublicofArtsakh,
+        Romania,
+        Russia,
+        Rwanda,
+        Saba,
+        SahrawiArabDemocraticRepublic,
+        SaintBarthélemy,
+        SaintHelenaAscensionAndTristanDaCunha,
+        SaintKittsAndNevis,
+        SaintLucia,
+        SaintMartin,
+        SaintPierreAndMiquelon,
+        SaintVincentAndTheGrenadines,
+        Samoa,
+        SanMarino,
+        SãoToméAndPríncipe,
+        SaudiArabia,
+        Senegal,
+        Serbia,
+        Seychelles,
+        SierraLeone,
+        Singapore,
+        SintEustatius,
+        SintMaarten,
+        Slovakia,
+        Slovenia,
+        SolomonIslands,
+        Somalia,
+        Somaliland,
+        SouthAfrica,
+        SouthGeorgiaAndTheSouthSandwichIslands,
+        SouthKorea,
+        SouthOssetia,
+        SouthSudan,
+        Spain,
+        SpratlyIslands,
+        SriLanka,
+        Sudan,
+        Suriname,
+        Svalbard,
+        Sweden,
+        Switzerland,
+        Syria,
+        Taiwan,
+        Tajikistan,
+        Tanzania,
+        Thailand,
+        Togo,
+        Tokelau,
+        Tonga,
+        Transnistria,
+        TrinidadAndTobago,
+        Tunisia,
+        Turkey,
+        Turkmenistan,
+        TurksAndCaicosIslands,
+        Tuvalu,
+        USVirginIslands,
+        Uganda,
+        Ukraine,
+        UnitedArabEmirates,
+        UnitedKingdom,
+        UnitedStates,
+        UnitedStatesMinorOutlyingIslands,
+        Uruguay,
+        Uzbekistan,
+        Vanuatu,
+        VaticanCity,
+        Venezuela,
+        Vietnam,
+        WallisAndFutuna,
+        Yemen,
+        Zambia,
+        Zimbabwe,
+    ]
+
+    for country in countries:
+        db.session.add(country)
+
+    db.session.commit()
+
+
+# Uses a raw SQL query to TRUNCATE the countries table.
+# SQLAlchemy doesn't have a built in function to do this
+# TRUNCATE Removes all the data from the table, and RESET IDENTITY
+# resets the auto incrementing primary key, CASCADE deletes any
+# dependent entities
+def undo_countries():
+    db.session.execute('TRUNCATE countries RESTART IDENTITY CASCADE;')
+    db.session.commit()
