e41a7129fa9cf9d938a6aa785eca3385d08874a4
cody <cody.brown1993@gmail.com>
2021-07-22 11:00:27 -0400

    dynamic backings ready for rewards

--- app/api/backing_routes.py
+++ app/api/backing_routes.py
@@ -30,10 +30,13 @@ def add_backing():
     form['reward_id'].data = request.json['backing']['reward_id']
     form['amount'].data = request.json['backing']['amount']
 
-    filteredBackings = Backing.query.filter((Backing.user_id == form['user_id'].data), (Backing.project_id == form['project_id'].data)).all()
+    # testIng = Backing.query.filter((Backing.reward_id == None)).all()
+    # print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&',testIng)
+
+    filteredBackings = Backing.query.filter((Backing.user_id == form['user_id'].data), (Backing.project_id == form['project_id'].data), (Backing.reward_id == None)).all()
     # print("HERE ARE THE THINGS", type(form['project_id'].data),type(form['user_id'].data) )
     # print(form['user_id'].data)
-    # print('************************************** BACK', filteredBackings[0].id)
+    print('************************************** BACK', filteredBackings)
 
     if form.validate_on_submit():
 
@@ -44,7 +47,7 @@ def add_backing():
             db.session.add(filteredBackings[0])
             db.session.commit()
             proj_to_update = Project.query.get(form['project_id'].data)
-            
+
             proj_to_update.current_funding += form['amount'].data
             db.session.add(proj_to_update)
             db.session.commit()
