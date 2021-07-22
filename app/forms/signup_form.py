18aa9ab7833c90fc2ee6d00ea94405ddfd1851b0
John Michihara <john.michihara@gmail.com>
2021-07-22 07:42:14 -1000

    Set up password check validation for sign up

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -4,24 +4,29 @@ from wtforms.validators import DataRequired, Email, ValidationError, InputRequir
 from app.models import User
 
 
-def user_exists(form, field):
+def user_email_exists(form, field):
     # Checking if user exists
     email = field.data
     user = User.query.filter(User.email == email).first()
     if user:
-        raise ValidationError('Email address is already in use.')
+        raise ValidationError('Email has already been taken.')
 
 
-def username_exists(form, field):
-    # Checking if username is already in use
-    username = field.data
-    user = User.query.filter(User.username == username).first()
-    if user:
-        raise ValidationError('Username is already in use.')
+# def username_exists(form, field):
+#     # Checking if username is already in use
+#     username = field.data
+#     user = User.query.filter(User.username == username).first()
+#     if user:
+#         raise ValidationError('Username is already in use.')
 
 
 class SignUpForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired(), InputRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), InputRequired(), Email(message='Please provide an valid email', check_deliverability=True), user_exists])
-    password = StringField('password', validators=[DataRequired(), InputRequired()]) # EqualTo("confirm_password", message="password must match confirm passord")
+    username = StringField('username', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired(), Email(
+        message='Please provide a valid email.', check_deliverability=True), user_email_exists])
+    password = StringField('password', validators=[InputRequired(), EqualTo(
+        'repeat_password', message='Password confirmation doesn\'t match Password')])
+    repeat_password = StringField('repeatPassword')
+
+    # EqualTo("confirm_password", message="password must match confirm passord")
     #confirm_password = StringField('confirm_password', validators=[])
