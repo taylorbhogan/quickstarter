6bdbf0d490e040e12e30a47dabb13c2d635f02b8
John Michihara <john.michihara@gmail.com>
2021-07-23 16:53:14 -1000

    Modularize the featured component

---
+++ react-app/src/components/ViewComponents/Featured/Recommended2.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import RecommendedProject from './RecommendedProject';
+import styles from './Featured.module.css';
+
+const Recommended2 = ({ projects, openSecond, setOpenFirst, setOpenSecond, setOpenThird }) => {
+
+  const getRandomNum = (max) => {
+    return Math.floor(Math.random() * max);
+  };
+
+  const getRecommendedProjects = (projects, max) => {
+    const recommended = [];
+    for (let i = 0; i < max; i++) {
+      recommended.push(projects[getRandomNum(projects.length)]);
+    }
+    return recommended;
+  };
+
+  const handleClick = (setOpenFunction, setCloseFunction) => {
+    setOpenFunction(true);
+    setCloseFunction(false);
+  };
+
+  return (
+    <>
+      {openSecond &&
+        (<div className={styles.right}>
+          <h3 className={styles.title}>RECOMMENDED FOR YOU</h3>
+          <div className={styles.recommendedContainer}>
+            {projects.length > 0 && getRecommendedProjects(projects, 3).map(project => (
+              <NavLink className={styles.linkWrapper} to={`projects/${project.id}`}>
+                <RecommendedProject project={project} />
+              </NavLink>
+            ))}
+            <div className={styles.pageButtonsContainer}>
+              <span className={styles.pageArrow} onClick={() => handleClick(setOpenFirst, setOpenSecond)}>
+                <i class="fas fa-chevron-left"></i>
+              </span>
+              <span className={styles.pageButton} onClick={() => handleClick(setOpenFirst, setOpenSecond)}>1</span>
+              <span className={styles.pageButtonSelected}>2</span>
+              <span className={styles.pageButton} onClick={() => handleClick(setOpenThird, setOpenSecond)}>3</span>
+              <span className={styles.pageArrow} onClick={() => handleClick(setOpenThird, setOpenSecond)}>
+                <i class="fas fa-chevron-right"></i>
+              </span>
+            </div>
+          </div>
+        </div>)
+      }
+    </>
+  );
+};
+
+export default Recommended2;
---
+++ react-app/src/components/ViewComponents/Featured/Recommended3.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import RecommendedProject from './RecommendedProject';
+import styles from './Featured.module.css';
+
+const Recommended3 = ({ projects, openThird, setOpenFirst, setOpenSecond, setOpenThird }) => {
+
+  const getRandomNum = (max) => {
+    return Math.floor(Math.random() * max);
+  };
+
+  const getRecommendedProjects = (projects, max) => {
+    const recommended = [];
+    for (let i = 0; i < max; i++) {
+      recommended.push(projects[getRandomNum(projects.length)]);
+    }
+    return recommended;
+  };
+
+  const handleClick = (setOpenFunction, setCloseFunction) => {
+    setOpenFunction(true);
+    setCloseFunction(false);
+  };
+
+  return (
+    <>
+      {openThird &&
+        (<div className={styles.right}>
+          <h3 className={styles.title}>RECOMMENDED FOR YOU</h3>
+          <div className={styles.recommendedContainer}>
+            {projects.length > 0 && getRecommendedProjects(projects, 3).map(project => (
+              <NavLink className={styles.linkWrapper} to={`projects/${project.id}`}>
+                <RecommendedProject project={project} />
+              </NavLink>
+            ))}
+            <div className={styles.pageButtonsContainer}>
+              <span className={styles.pageArrow} onClick={() => handleClick(setOpenSecond, setOpenThird)}>
+                <i class="fas fa-chevron-left"></i>
+              </span>
+              <span className={styles.pageButton} onClick={() => handleClick(setOpenFirst, setOpenThird)}>1</span>
+              <span
+                className={styles.pageButton} onClick={() => handleClick(setOpenSecond, setOpenThird)}> 2</span>
+              <span className={styles.pageButtonSelected}>3</span>
+              <span className={styles.pageArrowOff}>
+                <i class="fas fa-chevron-right"></i>
+              </span>
+            </div>
+          </div>
+        </div>)
+      }
+    </>
+  );
+};
+
+export default Recommended3;
