4d3d22cf5dd112d87b8e2728e862ad7436397228
Taylor Hogan <taylorb.hogan@gmail.com>
2021-07-24 23:51:19 -0700

    Hook up Discover modal

--- react-app/src/components/Discover/index.js
+++ react-app/src/components/Discover/index.js
@@ -1,32 +1,31 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { getProjects } from '../../store/project';
-import { NavLink } from 'react-router-dom';
+import DiscoverList from './DiscoverList';
+import styles from './Discover.module.css'
 
 
-
-function Discover() {
+function Discover({setShowDiscoverModal}) {
   const dispatch = useDispatch();
 
   const projects = useSelector(state => Object.values(state.projects))
 
+  const handleClick = () => {
+    setShowDiscoverModal(false)
+  }
 
   useEffect(() => {
     dispatch(getProjects())
   }, [dispatch])
 
-  const projectComponents = projects.map((project) => {
-    return (
-      <li key={project.id}>
-        <NavLink to={`/projects/${project.id}`}>{project.subTitle}</NavLink>
-      </li>
-    );
-  });
   return (
-    <div>
-      <h1>Hello from discover</h1>
-      <h2>Project List: </h2>
-      <ul>{projectComponents}</ul>
+    <div className={styles.document}>
+      <button
+        onClick={handleClick}
+      >Close</button>
+      <DiscoverList
+        projects={projects}
+      />
     </div>
   )
 }
