61befb705d575004028f6b1f7d1bad0806e763ea
Taylor Hogan <taylorb.hogan@gmail.com>
2021-07-25 10:12:27 -0700

    Style 404 page

--- react-app/src/components/PageNotFound/index.js
+++ react-app/src/components/PageNotFound/index.js
@@ -1,16 +1,35 @@
 import styles from './PageNotFound.module.css'
+import { NavLink } from 'react-router-dom'
+import { useEffect } from 'react'
 
 function PageNotFound() {
+
+  // useEffect(()=>{
+  //   const randomNumber = Math.floor(Math.random() * 100)
+  // },[])
+
   return (
-    <div>
-      <div>QUICKSTARTER</div>
-      <div>Back it up!</div>
-      <h2>We can’t find this page, but we can show you a new creative project you can help bring to life.</h2>
-      <div>
-        <button>Take a chance</button>
-        <button>Take me home</button>
+    <div className={styles.bigBucket}>
+      <div className={styles.left}>
+        <div className={styles.logoPlaceholder}>QUICKSTARTER</div>
+        <div>
+          <div className={styles.header}>Back it up!</div>
+          <div className={styles.blurb}>We can’t find this page, but we can show you a new creative project you can help bring to life.</div>
+          <div className={styles.buttons}>
+            {/* <NavLink to={`/projects/${randomNumber}`} exact={true}> */}
+              <button className={styles.buttonBlack}>Take a chance</button>
+            {/* </NavLink> */}
+            <NavLink to='/' exact={true}>
+              <button className={styles.buttonWhite}>Take me home</button>
+            </NavLink>
+          </div>
+          <div className={styles.cta}>Check our status blog for updates</div>
+
+        </div>
       </div>
-      <div>Check our status blog for updates</div>
+      <div className={styles.right}>
+        <div className={styles.imageDiv}></div>
+      </div>
     </div>
   )
 }
