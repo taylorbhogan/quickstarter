5a3b0820b66abf31e233e3bc6399790696803154
Taylor Hogan <taylorb.hogan@gmail.com>
2021-08-25 20:23:23 -0400

    Complete paymentEdit styling

--- react-app/src/components/ProjectEdit/ProjectEditPromotion/index.js
+++ react-app/src/components/ProjectEdit/ProjectEditPromotion/index.js
@@ -14,7 +14,7 @@ function ProjectEditPromotion() {
               <div className={promotionStyles.mainInputHeader}>Project URL</div>
               <div className={promotionStyles.mainInputSubHeader}>We'll use your current project title to generate a URL. Even if you change the title later, the URL will stay the same. Your project page won't go live until you launch.</div>
             </div>
-            <div className={promotionStyles.standIn}></div>
+            <div className={promotionStyles.locked}></div>
           </section>
         </div>
         <div className={promotionStyles.blockWrapper}>
@@ -23,7 +23,7 @@ function ProjectEditPromotion() {
               <div className={promotionStyles.mainInputHeader}>Pre-launch page</div>
               <div className={promotionStyles.mainInputSubHeader}>Your project title, subtitle, and category from the Basics section will automatically appear on this page. Potential backers will be able to find it on Kickstarter, but you should share your pre-launch page with everyone to create excitement and attention around your project before you launch it.</div>
             </div>
-            <div className={promotionStyles.standIn}></div>
+            <div className={promotionStyles.locked}></div>
           </section>
         </div>
         <div className={promotionStyles.blockWrapper}>
@@ -32,7 +32,7 @@ function ProjectEditPromotion() {
               <div className={promotionStyles.mainInputHeader}>Google Analytics</div>
               <div className={promotionStyles.mainInputSubHeader}>This powerful tracking tool offers insight into your project’s performance, including the number of visitors to your page, where they came from, and how many of them convert into pledges.</div>
             </div>
-            <div className={promotionStyles.standIn}></div>
+            <div className={promotionStyles.locked}></div>
           </section>
         </div>
       </div>
