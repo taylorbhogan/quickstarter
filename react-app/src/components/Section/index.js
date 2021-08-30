ff356bce58b4f9a7171a0a18a04fe728363a26d0
John Michihara <john.michihara@gmail.com>
2021-08-30 09:09:54 -1000

    Remove subscribe components since there is no functionality

--- react-app/src/components/Section/index.js
+++ react-app/src/components/Section/index.js
@@ -1,44 +1,49 @@
-import React, { useEffect } from 'react';
-import { useParams, useHistory } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { getProjects } from '../../store/project';
-import { sections, filterProjectsBySection } from './sectionsData';
-import Header from '../ViewComponents/Header';
-import Featured from '../ViewComponents/Featured';
-import Explore from '../ViewComponents/Explore';
-import Subscribe from '../ViewComponents/Subscribe';
-import SectionsNavBar from '../SectionsNavBar';
+import React, { useEffect } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getProjects } from "../../store/project";
+import { sections, filterProjectsBySection } from "./sectionsData";
+import Header from "../ViewComponents/Header";
+import Featured from "../ViewComponents/Featured";
+import Explore from "../ViewComponents/Explore";
+// import Subscribe from '../ViewComponents/Subscribe';
+import SectionsNavBar from "../SectionsNavBar";
 
 const Section = () => {
   const dispatch = useDispatch();
   const history = useHistory();
   const { id } = useParams();
-  const projects = useSelector(state => Object.values(state.projects));
-  const [section] = sections.filter(section => {
+  const projects = useSelector((state) => Object.values(state.projects));
+  const [section] = sections.filter((section) => {
     return section.id === parseInt(id);
   });
   const filteredProjects = filterProjectsBySection(parseInt(id), projects);
-  console.log(filteredProjects)
+  console.log(filteredProjects);
 
   useEffect(() => {
-    dispatch(getProjects())
+    dispatch(getProjects());
   }, [dispatch]);
 
   return (
     <>
-      {(section && +id) ? (
+      {section && +id ? (
         <>
           <SectionsNavBar />
-          <Featured projects={filteredProjects.length > 0 ? filteredProjects : projects} />
+          <Featured
+            projects={filteredProjects.length > 0 ? filteredProjects : projects}
+          />
           <Header section={section} />
-          <Subscribe title={section.subscribeTitle} subtitle={section.subscribeSubtitle} />
+          {/* <Subscribe title={section.subscribeTitle} subtitle={section.subscribeSubtitle} /> */}
           <Explore
             projects={filteredProjects.length > 0 ? filteredProjects : projects}
-            section={section} />
+            section={section}
+          />
         </>
-      ) : history.replace('/404')}
+      ) : (
+        history.replace("/404")
+      )}
     </>
-  )
+  );
 };
 
 export default Section;
